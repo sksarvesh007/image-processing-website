6b6209c3a6ac4ff3d60b0b5823aea9cb3403eaf6
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-01-10 23:51:13 +0530

    added the water colouring feature and improved the UI

--- app.py
+++ app.py
@@ -15,13 +15,35 @@ def sharpen_image(image):
     sharpened = cv2.addWeighted(image, 1.5, gaussian_blur, -0.5, 0)
     return sharpened
 
+def water_art(image):
+    img_cleared = cv2.medianBlur(image, 5)
+    img_cleared=cv2.medianBlur(img_cleared, 5)
+    img_cleared=cv2.medianBlur(img_cleared, 5)
+    
+    img_cleared = cv2.edgePreservingFilter(img_cleared, flags=2, sigma_s=50, sigma_r=0.4)
+    
+    img_filtered = cv2.bilateralFilter(img_cleared, 3, 10, 5)
+    for i in range(2):
+        img_filtered = cv2.bilateralFilter(img_filtered, 3, 10, 5)
+        
+    for i in range(3):
+        img_filtered = cv2.bilateralFilter(img_filtered, 5 , 30 , 10)
+        
+    gaussian_mask = cv2.GaussianBlur(img_filtered, (7,7), 2)
+    image_sharp = cv2.addWeighted(img_filtered, 1.5, gaussian_mask, -0.5, 0)
+    image_sharp = cv2.addWeighted(image_sharp, 1.4, gaussian_mask, -0.2, 0)
+    return image_sharp
+
 def process_image(image, operation):
     if operation == 'blur':
         return blur_image(image)
     elif operation == 'sharpen':
         return sharpen_image(image)
+    elif operation == 'water':
+        return water_art(image)
     else:
         return image
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
